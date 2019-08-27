1adcf6b131a4e178d01959aa80b9c34c73a92a77
andrew stehno <andrew_stehno@yahoo.com>
2019-08-27 16:52:49 -0600

    psuedo code skeleton created in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+/* 
+    app randomly picks letter, user to guess which letter.
+-"what letter I'm thinking of"
+-wins
+-losses                     -game to start over without refresh
+-guesses left
+-guesses so far
+*/
+
+// variables go here:
+
+var randomNumer = x 
+var userGuess = y 
+var wins = 0
+var losses = 0
+var guessesLeft = 10
+var guessesSoFar = 0
+
+// game start code here:
+
+
+
+// generate random number/letter code here:
+
+
+
+// display user guess???  DOM manipulation???
+
+
+
+// conditionals code here:
