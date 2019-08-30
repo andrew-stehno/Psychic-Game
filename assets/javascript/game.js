25be9aaae92d882b1f03a30b9b19a6d158fe6e4c
andrew stehno <andrew_stehno@yahoo.com>
2019-08-29 22:05:00 -0600

    completed conditional statment for game ending and created rows/columns to aling heading with data

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,4 @@
-
-/* 
-    app randomly picks letter, user to guess which letter.
--"what letter I'm thinking of"
--wins
--losses                     -game to start over without refresh
--guesses left
--guesses so far
-*/
-
-//More copmuter SCeincy way
-//1. CHoosing a random number between two charCodes which represent the alphabet
-
-//Generate an array of all letters in the alphabet
-//More copmuter sciency way -- we can generate an array of all letters with a functoin
-//easier we can also write it by hand
-// variables go here:
-
+// variables:
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
@@ -29,15 +12,13 @@ var computerGuess = alphabet[randomNumber]
 document.onkeyup = function (event) {
     var userGuess = event.key;
     console.log("User guess " + userGuess);
-    //Is th user key actually a letter
+    //Is the user key actually a letter
     if (alphabet.indexOf(userGuess.toLowerCase()) > -1) {
 
-
+        // conditionals:
         if (computerGuess === userGuess.toLowerCase()) {
             wins++;
-            console.log(wins);
             guessesLeft = 10;
-            console.log(guessesLeft);
             guessesSoFar = [];
             computerGuess = alphabet[Math.floor((Math.random() * alphabet.length))];
         }
@@ -46,15 +27,17 @@ document.onkeyup = function (event) {
             function guessFunc() {
                 guessesSoFar.push(userGuess);    
                 document.getElementById("current-guesses").innerHTML = guessesSoFar;
-            console.log(guessesSoFar);
         }   guessFunc();
         }
 
         if (guessesLeft === 0) {
-            alert("You Lose!");
+            losses++
+//          alert("You Lose!");      Does this count as refreshing the screen?
+            guessesLeft = 10;
+            guessesSoFar = [];
         }
 
-        //After all this stuff we want to update all these values in the DOM :)
+        //manipulate the DOM:
         function updateDom() {
             var wins_1 = document.querySelector("#wins");
             wins_1.innerHTML = wins;
@@ -67,24 +50,12 @@ document.onkeyup = function (event) {
 
             var yourGuesses = document.querySelector("#current-guesses");
             yourGuesses.innerHTML = guessesSoFar;
-
         }
         updateDom(); 
-            
-       /* else {
-            alert("Input not valid, please enter a letter.")
-        }*/
+     /*       
+    else {
+        alert("Input not valid, please enter a letter.");
+    } */
 
-        //if so proceed
-        //is valid letter equal to computer guess
-        //if yes user wins (increase wins) -- restart game
-        //if no (decrement guesssleft)
-        //check to is guess left = 0 
-        //if yes increment losses
-        //if no do nothing 
-
-        //We will need to update DOM some where
-
-        //if not send failure to user
     }
 }
